09cc3ea3a2d33e2aa5aceb8de21c78fb4ef11cfa
ukzada <uk0501126@gmail.com>
2026-06-22 18:57:19 +0500

    add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello, git!");
+console.log("hello, i am learning CI/CD!");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("starting test");
+setTimeout(()=> consle.log("wait 3 sec"),3000);
+console.log("test complete!");
